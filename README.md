8e90c18f274de04dacf31be09a303f4534608b5c
Jansen Fuller <jansendfuller@mailfence.com>
2026-07-09 15:55:37 -0600

    Moving symbols around

--- README.md
+++ README.md
@@ -27,9 +27,9 @@ Configurations for three split keyboards sharing a 4-layer 5-column layout.
 ### Layer 1 — Lower + Numpad    (left thumb)
 
 ```
-  `     /     -     \                 =     7     8     9     +
+  .     /     -     \                 =     7     8     9     `
   [     (     ]     )                 *     4     5     6     '
-  <     {     >     }                 0     1     2     3     .
+  <     {     >     }                 0     1     2     3     +
 ```
 
 ### Layer 2 — Upper + Arrows    (right thumb)
--- qmk/keyboards/cheapino/keymaps/default/keymap.c
+++ qmk/keyboards/cheapino/keymaps/default/keymap.c
@@ -21,9 +21,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
 
     [NUM_SYM] = LAYOUT_split_3x5_3(
-        KC_GRV,  KC_SLSH, KC_MINS, KC_BSLS, KC_TRNS,      KC_EQL,  KC_7,    KC_8,    KC_9,    KC_PLUS,
+        KC_DOT,  KC_SLSH, KC_MINS, KC_BSLS, KC_TRNS,      KC_EQL,  KC_7,    KC_8,    KC_9,    KC_GRV,
         KC_LBRC, KC_LPRN, KC_RBRC, KC_RPRN, KC_TRNS,      KC_ASTR, KC_4,    KC_5,    KC_6,    KC_QUOT,
-        KC_LT,   KC_LCBR, KC_GT,   KC_RCBR, KC_TRNS,      KC_0,    KC_1,    KC_2,    KC_3,    KC_DOT,
+        KC_LT,   KC_LCBR, KC_GT,   KC_RCBR, KC_TRNS,      KC_0,    KC_1,    KC_2,    KC_3,    KC_PLUS,
                         KC_TRNS, KC_TRNS, KC_TRNS,         KC_TRNS, KC_TRNS, MO(3)
     ),
 
