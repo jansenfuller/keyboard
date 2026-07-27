7a2dd7320d7bc21244937e3a41eed10a3d83236b
Jansen Fuller <jansendfuller@mailfence.com>
2026-07-27 15:11:44 -0600

    lots of error fixes

--- qmk/keyboards/cheapino/rules.mk
+++ qmk/keyboards/cheapino/rules.mk
@@ -1,9 +1,6 @@
 CAPS_WORD_ENABLE = yes
 COMBO_ENABLE = yes
 CUSTOM_MATRIX = lite
-WS2812_DRIVER = vendor
-RGBLIGHT_ENABLE = yes
-DEFERRED_EXEC_ENABLE = yes
 SRC += encoder.c
 SRC += ghosting.c
 SRC += matrix.c
