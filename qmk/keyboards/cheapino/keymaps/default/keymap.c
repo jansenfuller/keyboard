e0631cbacec3ccb3183163d4853d30008567d146
Jansen Fuller <jansendfuller@mailfence.com>
2026-07-27 13:27:21 -0600

    fixing qmk workflow a and encoder

--- qmk/keyboards/cheapino/keymaps/default/keymap.c
+++ qmk/keyboards/cheapino/keymaps/default/keymap.c
@@ -37,8 +37,3 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                         KC_TRNS, KC_TRNS, KC_TRNS,         KC_TRNS, KC_TRNS, KC_TRNS
     ),
 };
-
-bool encoder_update_user(uint8_t index, bool clockwise) {
-    tap_code(clockwise ? KC_VOLU : KC_VOLD);
-    return true;
-}
