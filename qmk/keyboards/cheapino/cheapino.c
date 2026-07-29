c263b0c274ca79177103de5376b5c93386f0fc2f
Jansen Fuller <jansendfuller@mailfence.com>
2026-07-29 14:58:55 -0600

    Fixing led

--- qmk/keyboards/cheapino/cheapino.c
+++ qmk/keyboards/cheapino/cheapino.c
@@ -1,32 +1,22 @@
-#include "wait.h"
 #include "quantum.h"
 
 // Keep state between callbacks — when 0, the initial RGB flash is done
 uint8_t _hue_countdown = 50;
-
-// Track user-selected color so we can restore after flash
-uint8_t _hue;
-uint8_t _saturation;
-uint8_t _value;
 
 // 2.5 second color display on startup using deferred executor
 uint32_t flash_led(uint32_t next_trigger_time, void *cb_arg) {
     rgblight_sethsv(_hue_countdown * 5, 230, 70);
     _hue_countdown--;
     if (_hue_countdown == 0) {
-        // Restore user-chosen LED color
-        rgblight_sethsv(_hue, _saturation, _value);
+        // After startup flash, always switch to rainbow swirl
+        rgblight_sethsv(0, 255, 64);
+        rgblight_mode(RGBLIGHT_MODE_RAINBOW_SWIRL);
         return 0;
     }
     return 50;
 }
 
 void keyboard_post_init_user(void) {
-    // Store user-selected RGB HSV
-    _hue = rgblight_get_hue();
-    _saturation = rgblight_get_sat();
-    _value = rgblight_get_val();
-
-    // Flash colors on startup
+    // Flash colors on startup, then start rainbow
     defer_exec(50, flash_led, NULL);
 }
