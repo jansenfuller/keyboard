47a0c768a51881cec07010a93b7c36d39db57f8b
Jansen Fuller <jansen.fuller@lvt.com>
2026-07-30 14:02:18 -0600

    Fixing error

---
+++ qmk/keyboards/silakka54/keymaps/custom/config.h
@@ -0,0 +1,8 @@
+// Vial config for Silakka54
+#define VIAL_KEYBOARD_UID {0xFE, 0xED, 0x12, 0x12, 0x54, 0xCA, 0xFE, 0x01}
+#define VIAL_UNLOCK_COMBO_ROWS {0, 0}
+#define VIAL_UNLOCK_COMBO_COLS {0, 1}
+
+// Timing (homerow mods)
+#define TAPPING_TERM 170
+#define QUICK_TAP_TERM 120
